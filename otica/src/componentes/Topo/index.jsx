c4ee412222ee8e545fac28b93c449da6e1633ed8
Fabricio Jorge <fabricioj.dev@gmail.com>
2024-07-18 16:48:49 -0400

    adicionando js

--- otica/src/componentes/Topo/index.jsx
+++ otica/src/componentes/Topo/index.jsx
@@ -2,11 +2,50 @@ import Imagem from "next/image"
 import Logo from "../../../public/logo.png"
 import style from "@/componentes/Topo/Topo.module.css"
 import abrirMenu from "../../../public/abrir menu.png"
+import fecharMenu from '../../../public/fechar menu.png'
+
+
+
+function abrirModal(){
+    var mudarBotao = document.getElementById('botao')
+    var fecharBotao = document.getElementById('fechaBotao')
+    var modal = document.getElementById('modal')                 
+    modal.style.display = 'block'
+    mudarBotao.style.display = 'none'
+    fecharBotao.style.display = 'flex'
+}
+function fecharModal(){
+    var mudarBotao = document.getElementById('botao')
+    var fecharBotao = document.getElementById('fechaBotao')
+    var modal = document.getElementById('modal')                 
+    modal.style.display = 'none'
+    mudarBotao.style.display = 'flex'
+    fecharBotao.style.display = 'none'
+}
+
+// () => {
+//     var mudarBotao = document.getElementById('botao')
+//     var fecharBotao = document.getElementById('fechaBotao')
+//     var modal = document.getElementById('modal')
+//     var topo = document.getElementById('topo')
+//     if(topo.style.width >= '30%'){
+
+//         modal.style.display = 'none'
+//         mudarBotao.style.display = 'none'
+//         fecharBotao.style.display = 'none'
+//     }
+// }
+
+
+
+
+
 
 export default function Topo() {
+
     return(
-        <header className={style.topo}>
-            <div className={style.topoComponent}>
+        <header  className={style.topo}>
+            <div id="topo" className={style.topoComponent}>
                 <div >
                     <Imagem className={style.imagemTopo} src={Logo} alt="Logo da Otica"/>
                 </div>
@@ -18,13 +57,26 @@ export default function Topo() {
                     </ul>
                 </nav>
 
-                <div className={style.menuBotao} >
-                    <button className={style.botao}>
+                <div id="botao" className={style.menuBotao} >
+                    <button onClick={abrirModal} id="botao" className={style.botao}>
                         <Imagem className={style.abrir} src={abrirMenu} />
                     </button>
                 </div>
+                <div id="fechaBotao" className={style.fecharBotao} >
+                    <button onClick={fecharModal} id="botao" className={style.botao}>
+                        <Imagem className={style.abrir} src={fecharMenu} />
+                    </button>
+                </div>
+                {/* {
+                    let botao = document.getElementById('botao')
+                    if(botao.style.display == 'none'){
+                        modal.style.display = 'none'
+                    }
+                } */}
 
-                <nav className={style.modal}>
+                
+
+                <nav id="modal" className={style.modal}>
                     <ul>
                         <li><a href="#produto">Produto</a></li>
                         <li><a href="#sobre">Sobre</a></li>
@@ -33,6 +85,10 @@ export default function Topo() {
                 </nav>
 
             </div>
+
+            
         </header>
     )
 }
+
+// modalWidth()
